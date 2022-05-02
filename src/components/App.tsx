5834d1773281b52385aac6604c6de33b1b7795a7
Vivek Patel <patel.vivek@hackerrank.com>
2022-05-02 16:29:09 +0530

    comments with image or user's initials

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -204,6 +204,7 @@ import {
   SceneData,
   DeviceType,
   ActiveComment,
+  CommentOwner,
 } from "../types";
 import {
   debounce,
@@ -330,6 +331,8 @@ class App extends React.Component<AppProps, AppState> {
   public files: BinaryFiles = {};
   public imageCache: AppClassProperties["imageCache"] = new Map();
 
+  public excalOwner: CommentOwner;
+
   hitLinkElement?: NonDeletedExcalidrawElement;
   lastPointerDown: React.PointerEvent<HTMLCanvasElement> | null = null;
   lastPointerUp: React.PointerEvent<HTMLElement> | PointerEvent | null = null;
@@ -413,6 +416,31 @@ class App extends React.Component<AppProps, AppState> {
 
     this.actionManager.registerAction(createUndoAction(this.history));
     this.actionManager.registerAction(createRedoAction(this.history));
+
+    this.excalOwner = {
+      id: props.user?.email || this.id,
+      first_name: props.user?.first_name || "Anonymous",
+      email: props.user?.email || "Anonymous@hackerdraw.com",
+      color: props.user?.color || "#e64980",
+      ...props.user,
+    };
+
+    this.cacheCommentOwnerImage(this.excalOwner);
+  }
+
+  private cacheCommentOwnerImage(user: CommentOwner) {
+    if (user.image) {
+      // get image data from user.image url
+      const image = new Image();
+      image.src = user.image;
+      image.onload = () => {
+        this.imageCache.set(user.id as FileId, {
+          image,
+          mimeType: "image/png",
+        });
+      };
+      image.onerror = () => {};
+    }
   }
 
   private renderCanvas() {
@@ -818,7 +846,6 @@ class App extends React.Component<AppProps, AppState> {
         },
       };
     }
-
     const scene = restore(initialData, null, null);
     scene.appState = {
       ...scene.appState,
@@ -1229,7 +1256,6 @@ class App extends React.Component<AppProps, AppState> {
           canvasY !== this.state.activeComment.canvasY
         ) {
           activeComment = { element, canvasX, canvasY };
-          this.props.onActiveCommentUpdate?.(element, canvasX, canvasY);
         }
       }
       return (
@@ -3606,11 +3632,6 @@ class App extends React.Component<AppProps, AppState> {
                   canvasX,
                   canvasY,
                 };
-                this.props.onActiveCommentUpdate?.(
-                  hitElement,
-                  canvasX,
-                  canvasY,
-                );
               }
 
               this.setState((prevState) => {
@@ -3667,6 +3688,9 @@ class App extends React.Component<AppProps, AppState> {
     elementType: "comment",
     pointerDownState: PointerDownState,
   ): void => {
+    if (!this.props.user) {
+      return;
+    }
     const [gridX, gridY] = getGridPoint(
       pointerDownState.origin.x,
       pointerDownState.origin.y,
@@ -3677,6 +3701,7 @@ class App extends React.Component<AppProps, AppState> {
       type: elementType,
       x: gridX,
       y: gridY,
+      owner: this.excalOwner,
     });
 
     this.scene.replaceAllElements([
